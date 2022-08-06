5d93b7728f21241673d19fd5722b4e3c3da9e776
Alex Bellamy <alexjbellamy@gmail.com>
2022-08-06 13:45:34 -0400

    messing around with some bootstrap icon and other style stuff

---
+++ script.js
@@ -0,0 +1,24 @@
+// Variables
+var currentDay ; // moment variable for day display
+var hourlyTasks = {
+
+};
+
+
+
+// Element selectors
+var currentDayEl = $("#currentDay") ;
+
+
+
+// Functions
+
+
+
+
+
+
+
+
+
+// Main body
