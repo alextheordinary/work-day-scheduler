4b35b417f30359195d3684ca3ce2f83cded0f553
Alex Bellamy <alexjbellamy@gmail.com>
2022-08-06 15:53:38 -0400

    added pseudo-code. still figuring out overall architecture. implemented saveTask, retrieveTask, displayCalenderDate. started work on creatTimeBlockRow

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Variables
-var currentDay ; // moment variable for day display
-var hourlyTasks = {
-
-};
+var currentDay = moment() ; // moment variable for day display
+var currentHour = moment().format("k") ; // current hour in military time
+var hourlyTask = {
+    hour: 22,
+    task:"Sleep"
+}; // object template to store an hourly task. hour stored in military time. name convention is kk-task where k is the military time.
 
 
 
@@ -13,6 +15,35 @@ var currentDayEl = $("#currentDay") ;
 
 // Functions
 
+// Build out time blocks. Set labels to each hour. Apply past, present, future classes based on time comparison. Add a textarea to each timeblock. Fill textarea with any data from local storage for that hour. Add save button. Add event listener to each save button to save that textarea to localstorage.
+
+
+
+
+
+// Display today's date in the currentDayEl
+
+function displayCalendarDate() {
+    currentDayEl.text(currentDay.format("dddd, MMMM Do, YYYY")) ;
+}
+
+// Retrieve task from local storage for a given k time
+
+function retrieveTask(kTime) {
+    var storedTask = JSON.parse(localStorage.getItem(kTime + "-task"));
+    var taskText = "" ;
+    if (storedTask !== null) {
+       taskText = storedTask;
+    }
+    return taskText ;
+}
+
+// Save task to local storage for a given kk time.
+
+function saveTask(kTime, taskObject) {
+    localStorage.setItem(kTime + "-task", JSON.stringify(taskObject)) ;
+}
+
 
 
 
@@ -22,3 +53,6 @@ var currentDayEl = $("#currentDay") ;
 
 
 // Main body
+displayCalendarDate() ;
+
+
